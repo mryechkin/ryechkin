aa589a84a3e25d8c0ed294ea82911617bfa6299c
Mykhaylo Ryechkin <mykhaylo_ryechkin@manulife.ca>
2021-08-10 17:50:27 -0400

    fix(globals.css, Layout): Fixing typo and adjusting container padding

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,7 @@ export default function Layout({ children, isDark, setIsDark, hideKylo }) {
   return (
     <div className="absolute p-1 w-full min-h-screen bg-gradient-to-br dark:from-yellow-300 from-yellow-400 dark:to-cyan-400 to-cyan-500 dark:via-pink-400 via-pink-500 sm:p-2">
       <div className="min-h-[99vh] relative flex flex-col justify-around bg-gradient-to-br dark:from-gray-700 from-white to-gray-50 dark:to-gray-900">
-        <main className="dark:bg-pattern-dark bg-pattern-light prose md:prose-md lg:prose-lg xl:prose-xl z-10 px-2 w-full max-w-none bg-auto bg-left-top bg-no-repeat overflow-hidden sm:px-4 md:px-8">
+        <main className="dark:bg-pattern-dark bg-pattern-light prose md:prose-md lg:prose-lg xl:prose-xl z-10 px-4 w-full max-w-none bg-auto bg-left-top bg-no-repeat overflow-hidden sm:px-6 md:px-8">
           <div className="mx-auto max-w-4xl">{children}</div>
         </main>
         <Footer isDark={isDark} setIsDark={setIsDark} hideKylo={hideKylo} />
