0152bdd94315f0e7132314f7ac10f012b37526ba
Mykhaylo Ryechkin <5657181+mryechkin@users.noreply.github.com>
2021-09-12 19:25:53 -0400

    fix(Card): Fixing image layout issues on mobile

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,27 @@
 import { HiOutlineExternalLink } from 'react-icons/hi';
 import { motion } from 'framer-motion';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Card({ item, isExternal }) {
   return (
     <Link href={item.href} passHref>
       <motion.a
-        className="retro-shadow focus grid grid-flow-row grid-cols-4 mt-6 max-h-full border-2 border-pink-500 rounded-lg overflow-hidden overflow-ellipsis sm:grid-flow-col sm:max-h-40"
+        className="retro-shadow focus grid grid-flow-row grid-cols-4 grid-rows-2 mt-6 max-h-full border-2 border-pink-500 rounded-lg overflow-hidden overflow-ellipsis sm:grid-flow-col sm:grid-rows-1 sm:max-h-48"
         whileFocus={{ y: -3 }}
         whileHover={{ y: -3 }}
         whileTap={{ y: 0 }}
       >
-        <img
-          src={item.imageUrl}
-          className="col-span-4 w-full h-full object-cover sm:col-span-1"
-          alt={item.title}
-        />
+        <div className="relative col-span-4 w-full h-full sm:col-span-1">
+          <Image
+            src={item.imageUrl}
+            alt={item.title}
+            width={1280}
+            height={720}
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
         <div className="flex flex-col col-span-4 items-start justify-start p-4 bg-gray-50 dark:bg-gray-900 sm:col-span-3">
           <h1 className="inline-flex flex-nowrap items-center justify-start text-left dark:text-cyan-300 text-cyan-500 text-lg font-bold overflow-ellipsis sm:text-xl md:text-2xl">
             <span className="line-clamp-1">{item.title}</span>
