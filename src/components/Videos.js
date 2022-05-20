95bb792fe44a9f666b91c733fca7606011a1fd3b
Mykhaylo Ryechkin <5657181+mryechkin@users.noreply.github.com>
2022-05-20 15:44:10 -0400

    chore(Videos): Adding React Live code editor video

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,8 +1,20 @@
 import Card from './Card';
 
-const items = [
+const data = [
+  {
+    title: 'Recreating code editor from Chakra UI docs using React Live and TailwindCSS',
+    date: '2022-05-20',
+    duration: '43:18',
+    href: 'https://youtu.be/Ld3W9aaNx1g',
+    summary:
+      'Recreate the live code editor from Chakra UI, built using React Live and TailwindCSS.',
+    imageUrl: '/thumbs/live-code-editor.jpg',
+    tags: ['Chakra UI', 'React Live', 'TailwindCSS'],
+  },
   {
     title: 'ESLint + Prettier shared config for Next.js',
+    date: '2021-05-15',
+    duration: '20:07',
     href: 'https://youtu.be/tsPXN4mJGSc',
     summary:
       'Learn how to create and publish a custom ESLint and Prettier shared config, for use in Next.js projects!',
@@ -11,6 +23,8 @@ const items = [
   },
   {
     title: 'HeadlessUI Slideover',
+    date: '2021-04-20',
+    duration: '12:01',
     href: 'https://youtu.be/7GAvtWmVRSY',
     summary:
       'In this video we look at how to update the existing implementation of the Tailwind UI Slide Over component that I showed in an earlier video, with the ready-made React component that is now available out of the box from Tailwind UI.',
@@ -19,6 +33,8 @@ const items = [
   },
   {
     title: 'Icon Library with SVGR and Rollup',
+    date: '2021-02-23',
+    duration: '21:23',
     href: 'https://youtu.be/v0ZLEy1SE-A',
     summary:
       'Learn how to build your own SVG icon component library for React! In this video, we use SVGR and Rollup to make a publishable NPM package, with components generated directly from your SVG files.',
@@ -27,6 +43,8 @@ const items = [
   },
   {
     title: 'Accessible SlideOver with Tailwind UI and React-Aria',
+    date: '2021-01-21',
+    duration: '53:36',
     href: 'https://youtu.be/9EqJ-xgmIHc',
     summary:
       'In this video I show you everything you need to build the "Slide Over" from Tailwind UI as a fully accessible React component, using Tailwind CSS, React-Aria and Framer Motion.',
@@ -36,5 +54,5 @@ const items = [
 ];
 
 export default function Videos() {
-  return items.map((item) => <Card key={item.imageUrl} item={item} isExternal isVideo />);
+  return data.map((item) => <Card key={item.imageUrl} item={item} isExternal isVideo />);
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,52 +10,10 @@ import {
   RickRoll,
   SEO,
   Separator,
+  Videos,
 } from '@/components';
 import { getAllPosts } from '@/lib/data';
 import { useConfetti } from '@/lib/hooks';
-
-const videos = [
-  {
-    title: 'ESLint + Prettier shared config for Next.js',
-    date: '2021-05-15',
-    duration: '20:07',
-    href: 'https://youtu.be/tsPXN4mJGSc',
-    summary:
-      'Learn how to create and publish a custom ESLint and Prettier shared config, for use in Next.js projects!',
-    imageUrl: '/thumbs/eslint-prettier-config.jpg',
-    tags: ['ESLint', 'Prettier', 'Next.js'],
-  },
-  {
-    title: 'HeadlessUI Slideover',
-    date: '2021-04-20',
-    duration: '12:01',
-    href: 'https://youtu.be/7GAvtWmVRSY',
-    summary:
-      'In this video we look at how to update the existing implementation of the Tailwind UI Slide Over component that I showed in an earlier video, with the ready-made React component that is now available out of the box from Tailwind UI.',
-    imageUrl: '/thumbs/headlessui-slideover.jpg',
-    tags: ['Headless UI', 'Tailwind UI', 'TailwindCSS'],
-  },
-  {
-    title: 'Icon Library with SVGR and Rollup',
-    date: '2021-02-23',
-    duration: '21:23',
-    href: 'https://youtu.be/v0ZLEy1SE-A',
-    summary:
-      'Learn how to build your own SVG icon component library for React! In this video, we use SVGR and Rollup to make a publishable NPM package, with components generated directly from your SVG files.',
-    imageUrl: '/thumbs/svgr-icon-library.jpg',
-    tags: ['Rollup', 'SVGR', 'Libraries'],
-  },
-  {
-    title: 'Accessible SlideOver with Tailwind UI and React-Aria',
-    date: '2021-01-21',
-    duration: '53:36',
-    href: 'https://youtu.be/9EqJ-xgmIHc',
-    summary:
-      'In this video I show you everything you need to build the "Slide Over" from Tailwind UI as a fully accessible React component, using Tailwind CSS, React-Aria and Framer Motion.',
-    imageUrl: '/thumbs/accessible-slideover.jpg',
-    tags: ['React Aria', 'Tailwind UI', 'Accessibility', 'a11y'],
-  },
-];
 
 export default function Home({ posts }) {
   const [confetti] = useConfetti();
@@ -93,9 +51,7 @@ export default function Home({ posts }) {
         <Separator />
         <h1 className="title">Videos</h1>
         <div className="pb-4 md:pb-8">
-          {videos.map((item) => (
-            <Card key={item.imageUrl} item={item} isExternal isVideo />
-          ))}
+          <Videos />
         </div>
         <Separator />
         <div className="inline-flex items-center justify-center w-full">
