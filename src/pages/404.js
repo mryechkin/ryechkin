8272dd8603e3850b07df24c2c84f68fc6f3d8f54
Mykhaylo Ryechkin <5657181+mryechkin@users.noreply.github.com>
2023-01-03 14:06:48 -0500

    chore(blog/nextjs-supabase-auth): Fixing sample code; small UI tweaks

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,24 +1,27 @@
 import { FaExclamationTriangle } from 'react-icons/fa';
 import Link from 'next/link';
 
+import HeroContainer from 'src/components/HeroContainer';
 import Layout from 'src/components/Layout';
 
 export default function NotFound() {
   return (
     <Layout>
       <div className="prose mx-auto pb-8 text-center sm:mt-20 md:prose-lg lg:prose-xl">
-        <div className="flex items-center justify-center p-8">
-          <FaExclamationTriangle className="h-10 w-10 text-rose-500 dark:text-rose-400" />
+        <div className="flex flex-col items-center justify-center gap-4">
+          <div className="flex items-center justify-center p-8">
+            <FaExclamationTriangle className="h-10 w-10 text-rose-500 dark:text-rose-300" />
+          </div>
+          <h1 className="retro">Page Not Found.</h1>
+          <HeroContainer>
+            <p className="mx-auto max-w-5xl text-lg lg:text-xl">
+              These aren&apos;t the droids you&apos;re looking for.
+            </p>
+          </HeroContainer>
+          <Link className="mt-12" href="/" prefetch={false}>
+            Go Home
+          </Link>
         </div>
-        <h1 className="mt-4 text-4xl font-bold sm:text-5xl sm:tracking-tight lg:text-6xl">
-          Page Not Found.
-        </h1>
-        <p className="mx-auto mt-6 max-w-5xl text-base lg:text-lg">
-          These aren&apos;t the droids you&apos;re looking for.
-        </p>
-        <Link href="/" prefetch={false}>
-          &larr; Home
-        </Link>
       </div>
     </Layout>
   );
