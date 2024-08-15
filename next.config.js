6935129dc30106af2060e3da9fd3fcd2635a7099
Mykhaylo Ryechkin <5657181+mryechkin@users.noreply.github.com>
2024-08-15 17:28:51 -0400

    fix(nextConfig): Add outputFileTracingIncludes for search

--- next.config.js
+++ next.config.js
@@ -5,4 +5,9 @@
 module.exports = {
   reactStrictMode: false,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  experimental: {
+    outputFileTracingIncludes: {
+      '/search': ['./src/data/blog/*'],
+    },
+  },
 };
